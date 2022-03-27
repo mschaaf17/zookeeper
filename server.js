b802350102076c1ab1ff8c9007f111a78ed48873
Maddy <madisonschaaf48@gmail.com>
2022-03-26 18:50:09 -0600

    added post with validation

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 //if after npm start the server has not reset do ctrl c and then y
 
+//need to be added to write the new animals into the json file from the POST request
+const fs = require('fs')
+const path = require('path')
+
 const express = require('express')
 //this is creating a route that the front-end can request data from
 //its an object because animals as many differnt properties
 const { animals } = require('./data/animals')
 const PORT = process.env.PORT || 3001;
 const app = express()
+//this will intercept our POST request before it gets to the callback function since it won't be able to read it
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }))
+//parse incoming JSON data
+app.use(express.json())
 
 //this function should take req.query as an argument and filter through the animals, returning the new filtered array
 //this will be called in the app.get() function
@@ -58,6 +67,45 @@ function findById(id, animalsArray) {
   return result
 }
 
+//this function will accept the POST routes req.body value and the array we want to add data to which will be animalsArray since the function is for adding a new animal to the catalog
+function createNewAnimal(body, animalsArray) {
+//  console.log(body)
+  const animal = body
+  animalsArray.push(animal)
+  //write to save into the animals.json file
+  //sync does not require a callback if the data set was larger use async
+  //we are writing out animals.json file in the data subdirectory so we use the method path.join() to join the value of __dirname, which represents the directory of the file we execute the code in, which the path to the animals.json file
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    //this is to save the javascript array data as json, null and 2 means of keeping out data formatted 
+    //null means we dont want to edit any of our exciting data, 2 we want to create white space between out values to make them more readable
+    //leave out null or 2 it will just be hard to read
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  )
+  //return finished code to post route for response we will change return body to return animal since we have a const
+  //when we POST a new animal we will add it to the imported animals array from the animals.json file
+  return animal;
+}
+
+//this is going to take our new animal data from req.body and check if each key not only exitst but that it also the right type of data
+//after this function we need to make sure the POST routes callback is checking this before creating the data
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
+
 
 //gets are your routes for user to get access to the json file
 //adding the route-get requires 2 arugments-1st string that describes the route the client wil have to fetch from, the second is the clalback function that will execute every time the route is accessed with a GET request
@@ -91,6 +139,32 @@ app.get('/api/animals/:id', (req, res) => {
     res.sendStatus(404)
   }
 })
+
+//this is defining a route that listens to the POST request got GET requests-- POST differt from GET in that they represent the action of a client requesting the server to accept data rather than vice versa
+//this just allows us to accept the request 
+app.post('/api/animals', (req, res) => {
+  //this will be seeting up the post request
+  //req.body is where our incoming content will be-- POST we can package up data, typically as an object, and send it to the server
+  //req.body is where we can access that data on the server side and do something with it. 
+  //console.log will view the data were posting to the server and then res.json to send the data back to the client
+  console.log(req.body)
+  //set id based on what the next index of the array will be
+  //test this in insomnia 
+  req.body.id = animals.length.toString()
+
+  //validation check from function
+  //if any data in req.body is inccorect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    //resstatussend is a reponse method to relay a message to the client making a request 404 means user error not server error
+    res.status(400).send('This animal is no properly formatted.')
+  } else {
+  //add animal to json file and animals array in this function
+  //once we add this const we are change res.json(req.body) to res.json(animal)
+  //the const animal now has req.body as well as animals
+  const animal = createNewAnimal(req.body, animals)
+    res.json(animal)
+  }
+  })
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
